12b7df01f71bdf8cfcb0a7c84da50638666eceed
imen jmal <imen jmal>
2016-12-14 03:21:06 +0100

    popUp add a group

--- app/src/main/java/com/example/imen/familyshoppinglist/HomeActivity.java
+++ app/src/main/java/com/example/imen/familyshoppinglist/HomeActivity.java
@@ -1,15 +1,25 @@
 package com.example.imen.familyshoppinglist;
 
 import android.content.Intent;
+import android.graphics.Point;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -29,6 +39,8 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     private RecyclerView recyclerView;
     private MyAdapter adapter;
     ArrayList<String> name ;
+    String[] DayOfWeek = {"Sunday", "Monday", "Tuesday",
+            "Wednesday", "Thursday", "Friday", "Saturday"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,9 +48,7 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_home);
         setTitle("test");    //+user.getEmail()
         name=new ArrayList<>();
-        name.add("imen");
-        name.add("sisters");
-        name.add("mum");
+
         recyclerView= (RecyclerView) findViewById(R.id.recycler_view);
         adapter=new MyAdapter(this,name);
         recyclerView.setAdapter(adapter);
@@ -71,13 +81,65 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-              name.add("cccccccccccc");
+
+                LayoutInflater layoutInflater =
+                        (LayoutInflater)getBaseContext()
+                                .getSystemService(LAYOUT_INFLATER_SERVICE);
+                View popUpView = layoutInflater.inflate(R.layout.popup_layout, null);
+                final PopupWindow popupWindow;
+                popupWindow = new PopupWindow(popUpView, RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT, true);
+                popupWindow.setBackgroundDrawable(new BitmapDrawable(null, ""));
+                popupWindow.setAnimationStyle(android.R.style.Animation_Dialog);
+                popupWindow.showAtLocation(popUpView, Gravity.CENTER, 0, 0);
+               /* View popupView = layoutInflater.inflate(R.layout.popup_layout, null);
+                final PopupWindow popupWindow = new PopupWindow(
+                        popupView, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+
+                //Update TextView in PopupWindow dynamically
+                String stringOut ="test"; //textIn.getText().toString();
+                if(!stringOut.equals("")){
+                    textOut.setText(stringOut);
+                }*/
+
+                final TextView textOut = (TextView)popUpView.findViewById(R.id.groupName);
+
+                Button createButton = (Button)popUpView.findViewById(R.id.create);
+                Button cancelButton = (Button)popUpView.findViewById(R.id.cancel);
+
+                Spinner popupSpinner = (Spinner)popUpView.findViewById(R.id.popupspinner);
+
+                final ArrayAdapter<String> adapter =
+                        new ArrayAdapter<String>(HomeActivity.this,
+                                android.R.layout.simple_spinner_item, DayOfWeek);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                popupSpinner.setAdapter(adapter);
+
+                createButton.setOnClickListener(new Button.OnClickListener(){
+
+                    @Override
+                    public void onClick(View v) {
+                        name.add(textOut.getText().toString());
+                        adapter.notifyDataSetChanged();
+                        popupWindow.dismiss();
+                    }});
+                cancelButton.setOnClickListener(new Button.OnClickListener(){
+
+                    @Override
+                    public void onClick(View v) {
+                        popupWindow.dismiss();
+                    }});
                 adapter.notifyDataSetChanged();
+                popupWindow.showAsDropDown(fab,50,100); //AsDropDown(btnOpenPopup, 50, -30)
+                popupWindow.setFocusable(true);
+
+
             }
+
+
         });
     }
 
---
+++ app/src/main/java/com/example/imen/familyshoppinglist/ListActivity.java
@@ -0,0 +1,10 @@
+package com.example.imen.familyshoppinglist;
+
+import android.app.Activity;
+
+/**
+ * Created by imen on 14/12/2016.
+ */
+
+public class ListActivity extends Activity {
+}
